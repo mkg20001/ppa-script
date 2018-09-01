d7d43850abc6ce8be3db16a3182da1df19c995d2
Maciej Krüger <mkg20001@gmail.com>
2018-09-01 10:30:33 +0200

    feat: Add i686 => i386 alias

--- ppa-script.sh
+++ ppa-script.sh
@@ -5,7 +5,7 @@ set -e
 DISTS=""
 
 ARCHS="all amd64 arm64 armel armhf i386 mips mipsel mips64el ppc64el s390x" # https://www.debian.org/ports/
-OTHER_ARCHS="x64 x86 x86_64"
+OTHER_ARCHS="i686 x64 x86 x86_64"
 
 log() {
 #  echo "[$(date +%H:%M:%S)]: $*"
@@ -120,6 +120,9 @@ guess_arch() {
     ARCH="amd64"
   fi
   if [ "$ARCH" == "x86" ]; then
+    ARCH="i386"
+  fi
+  if [ "$ARCH" == "i686" ]; then
     ARCH="i386"
   fi
   if [ -z "$ARCH" ]; then
